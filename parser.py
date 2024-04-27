3ba15d6d82d3f7ac678bd66864829c0fdb0877a6
Dianas <diana.rytko@mail.ru>
2024-04-27 16:54:13 +0300

    init database

--- parser.py
+++ parser.py
@@ -19,15 +19,8 @@ DBPORT = env('DBPORT')
 
 
 class ParserAuto:
-    HEADERS = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-        'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
-        'Connection': 'keep-alive'
-    }
-    PROXIES = {
-        'http': 'http://50.172.75.127:80'
-    }
+    HEADERS = env('HEADERS')
+    PROXIES = env('PROXIES')
 
     @classmethod
     def get_soup(cls, url: str) -> BeautifulSoup:
